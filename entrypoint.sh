2e50820d4bd86dab748ab0b1c8354f5a306f4870
Sinaloense <sinaloensemel@gmail.com>
2025-12-05 17:17:08 -0700

    egg and run php-fpm as a daemon

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,14 +7,11 @@ nginx -v
 # Output Current PHP Version
 php --version
 
-# Create Nginx logs directory
-mkdir -p /home/container/nginx/logs
-
-# Create Nginx tmp directory
-mkdir -p /home/container/nginx/tmp
-
-# Create html directory
-mkdir -p /home/container/html
+# Run PHP-FPM
+php-fpm${PHP_VERSION} -c /home/container/fpm/php.ini --fpm-config /home/container/fpm/php-fpm.conf --daemonize \
+    || {
+        echo "ERROR: Failed to launch PHP-FPM."; exit 1;
+    }
 
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
